612920053c5b82184378fe119877a85f9e3c6377
rouilorouinb12 <rouilorouinb@gmail.com>
2025-06-13 22:24:14 +0800

    Add files via upload

---
+++ Program22.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] grades = new int[5, 3];
+
+        for (int student = 0; student < 5; student++)
+        {
+            for (int subject = 0; subject < 3; subject++)
+            {
+                grades[student, subject] = (student + 1) * 10 + subject * 5; 
+            }
+        }
+
+        Console.WriteLine("Grades of 5 students in 3 subjects:");
+        for (int student = 0; student < 5; student++)
+        {
+            Console.Write("Student " + (student + 1) + ": ");
+            for (int subject = 0; subject < 3; subject++)
+            {
+                Console.Write(grades[student, subject] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
